6d693199b09b04e6c8fba810228ecced4fdaf720
Enes Karakaş <enes.karakas.e@gamil.com>
2022-01-29 23:21:27 +0300

    linked list with sorter

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { StringCollection } from './StringCollection';
+import { LinkedList } from './LinkedList';
 StringCollection;
 
-const newNumCol = new NumbersCollection([10, 3, -2, -10]);
-
-const newStrCol = new StringCollection('tEstinGbeSt');
-
-// console.log(newNumCol.length);
+// const newNumCol = new NumbersCollection([10, 3, -2, -10]);
+// const sorter = new Sorter(newNumCol);
+// sorter.sort();
 // console.log(newNumCol.data);
 
-const sorter = new Sorter(newNumCol);
-const sorterStr = new Sorter(newStrCol);
-sorterStr.sort();
+// const newStrCol = new StringCollection('tEstinGbeSt');
+// const sorterStr = new Sorter(newStrCol);
+// sorterStr.sort();
+// console.log(newStrCol.data);
 
+const newLinkedList = new LinkedList();
+newLinkedList.add(500);
+newLinkedList.add(-20);
+newLinkedList.add(30);
+newLinkedList.add(-100);
+newLinkedList.add(99);
+const sorter = new Sorter(newLinkedList);
 sorter.sort();
-console.log(newNumCol.data);
-console.log(newStrCol.data);
+newLinkedList.print();
